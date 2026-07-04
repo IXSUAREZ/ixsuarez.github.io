4194b79d5f610452f6789c75740c0c6fb6c0e3c7
IXSUAREZ <diegoasuarez02@gmail.com>
2026-07-04 08:23:24 -0400

    Fix tablet browse menu placement

--- simply-endorsed/sw.js
+++ simply-endorsed/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "simply-endorsed-v18";
+const CACHE_NAME = "simply-endorsed-v19";
 
 const CORE_ASSETS = [
   "./",
   "./index.html",
   "./site.webmanifest?v=2",
-  "./css/app.css?v=26",
+  "./css/app.css?v=27",
   "./js/endorsements-data.js?v=3",
   "./js/browse-structure.js?v=3",
   "./js/guidance-content.js?v=3",
